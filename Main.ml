94529ed1e97dc590f61fdbb45c17e45ee2543694
JohnDoeJr <coprocodeforces@gmail.com>
2014-11-16 23:04:06 +0400

    Seems like it is working

--- Main.ml
+++ Main.ml
@@ -8,32 +8,55 @@ let rec print_term t =
 			print_string("("); print_term t1; print_string(")");
 			print_string("("); print_term t2; print_string(")");;
 
+let s = Lambda("x", Lambda("y", Lambda("z", Application(Application(Var "x", Var "z"), Application(Var "y", Var "z")))));;
+let k = Lambda("x", Lambda("y", Var "x"));;
 let x = Application(Lambda("x", Lambda("y", Lambda("z", Application(Application(Var "x", Var "z"), Application(Var "y", Var "z"))))), Lambda("x", Lambda("y", Var "x")));;
 let y = Application(Lambda("x", Lambda("y", Lambda("z", Application(Var "x", Application(Var "z", Application(Var "y", Var "z")))))), Lambda("x", Lambda("y", Var "x")));;
 let z = Application(Lambda("x", Application(Var "x", Var "x")), Lambda("x", Application(Var "x", Var "x")));;
+let w = Application(Lambda("x", Lambda("y", Lambda("z", Application(Application(Application(Var "x", Var "z"), Var "y"), Var "z")))), Lambda("x", Lambda("y", Var "x")));;
+let sksksk = Application(Application(Application(Application(Application(s, k), s), k), s), k);;
+let sk = Application(s, k);;
+let sks = Application(Application(Application(Application(sk, s), k), s), k);;
+
+let rec term_has_var term var =
+	match term with
+		| Var v -> if Var v = Var var then true else false
+		| Lambda (v, t) -> if Var v = Var var then true else term_has_var t v
+		| Application (t1, t2) -> (term_has_var t1 var) || (term_has_var t2 var);;
+
+let rec substituteVar term var =
+	match term with
+		| Var v -> if Var v = Var var then Var (v^"0") else Var v
+		| Lambda (v, t) -> if v = var then Lambda(v^"0", substituteVar t var) else Lambda(v, substituteVar t var)
+		| Application (t1, t2) -> Application(substituteVar t1 var, substituteVar t2 var);;
 
 let rec substitute t1 var t2 =
 	match t1 with
 		| Var v -> if Var v = Var var then t2 else Var v
-		| Lambda (v, t) -> Lambda (v, substitute t var t2)
+		| Lambda (v, t) -> 
+			if term_has_var t2 v 
+			then Lambda(v, substitute t var (substituteVar t2 v)) 
+			else Lambda(v, substitute t var t2)
 		| Application (t11, t12) -> Application (substitute t11 var t2, substitute t12 var t2);;
 
 let rec apply_term t1 t2 =
 	match t1 with
 		| Var v -> Application (t1, t2)
 		| Lambda (v, t) -> substitute t v t2
-		| Application (t11, t12) -> apply_term (apply_term t11 t12) t2;;
+		| Application (t11, t12) -> 
+			match t11, t12 with
+				| Var v1, t12 -> Application(t1, t2)
+				| _ -> apply_term (apply_term t11 t12) t2;;
+let rec is_term_normal term =
+	match term with
+		| Var v -> true
+		| Lambda (v, t) -> false
+		| Application (t1, t2) -> (is_term_normal t1) && (is_term_normal t2);;
 
 let rec normalize term =
-	print_endline "";
-	print_term term;
 	match term with
 		| Var v -> term
 		| Lambda (v, t) -> Lambda (v, normalize t)
-		| Application (t1, t2) -> 
-			match t1, t2 with
-				| Var v1, Var v2 -> Application(Var v1, Var v2)
-				| Var v1, t2 -> Application(Var v1, normalize t2)
-				| _ -> normalize (apply_term t1 t2);;
+		| Application (t1, t2) -> if is_term_normal term then term else normalize (apply_term t1 t2);;
 
-print_term(normalize y);;
+print_term(normalize (Application(sk, s)));;
